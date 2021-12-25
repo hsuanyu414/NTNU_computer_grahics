0c0d47363c3a9b137b99bc1965a3e2821fe9d150
HsuanYu <93172849+hsuanyu414@users.noreply.github.com>
2021-12-25 16:15:02 +0800

    fin_pro bumpMaping complete

--- Homework/final_pro/WebGL.js
+++ Homework/final_pro/WebGL.js
@@ -35,6 +35,7 @@ var FSHADER_SOURCE = `
         vec3 ambient = ambientLightColor * u_Ka;
         vec3 normal = normalize(v_Normal);
         vec3 v_LightPosition = (u_lightMatrix * u_LightPosition).xyz;
+        // vec3 v_LightPosition = (u_LightPosition).xyz;
         vec3 lightDirection = normalize(v_LightPosition - v_PositionInWorld);
         float nDotL = max(dot(lightDirection, normal), 0.0);
         vec3 diffuse = diffuseLightColor * u_Kd * nDotL;
@@ -293,6 +294,26 @@ function initVertexBufferForLaterUse(gl, vertices, normals, texCoords){
 
   return o;
 }
+
+function initVertexBufferForLaterUseBump(gl, vertices, normals, texCoords, tagents, bitagents, crossTexCoords){
+  var nVertices = vertices.length / 3;
+
+  var o = new Object();
+  o.vertexBuffer = initArrayBufferForLaterUse(gl, new Float32Array(vertices), 3, gl.FLOAT);
+  if( normals != null ) o.normalBuffer = initArrayBufferForLaterUse(gl, new Float32Array(normals), 3, gl.FLOAT);
+  if( texCoords != null ) o.texCoordBuffer = initArrayBufferForLaterUse(gl, new Float32Array(texCoords), 2, gl.FLOAT);
+  if( tagents != null ) o.tagentsBuffer = initArrayBufferForLaterUse(gl, new Float32Array(tagents), 3, gl.FLOAT);
+  if( bitagents != null ) o.bitagentsBuffer = initArrayBufferForLaterUse(gl, new Float32Array(bitagents), 3, gl.FLOAT);
+  if( crossTexCoords != null ) o.crossTexCoordsBuffer = initArrayBufferForLaterUse(gl, new Float32Array(crossTexCoords), 1, gl.FLOAT);
+  //you can have error check here
+  o.numVertices = nVertices;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  return o;
+}
+
 /////END://///////////////////////////////////////////////////////////////////////////////////////////////
 /////The folloing three function is for creating vertex buffer, but link to shader to user later//////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -360,7 +381,31 @@ var fox_objComponentIndex = ["./fox/fox_texture.png"];
 var fox_texCount = 0 ;
 var fox_numTextures = fox_imgNames.length;
 
-var cameraX = 0, cameraY = 0.68, cameraZ = 4;
+var steve_mvpMatrix;
+var steve_modelMatrix;
+var steve_normalMatrix;
+var steve_objScale = 0.1 ;
+var steve_objComponents = [];
+var steve_textures = {};
+var steve_imgNames = ["./steve/Steve_texture.png"];
+var steve_objComponentIndex = ["./steve/Steve_texture.png", "./steve/Steve_texture.png", "./steve/Steve_texture.png", "./steve/Steve_texture.png", "./steve/Steve_texture.png", "./steve/Steve_texture.png"];
+var steve_texCount = 0 ;
+var steve_numTextures = steve_imgNames.length;
+
+var dolphin_mvpMatrix;
+var dolphin_modelMatrix;
+var dolphin_normalMatrix;
+var dolphin_objScale = 0.1 ;
+var dolphin_objComponents = [];
+var dolphin_textures = {};
+var dolphin_imgNames = ["./dolphin/dolphin_texture.jpg"];
+var dolphin_objComponentIndex = ["./dolphin/dolphin_texture.jpg"];
+var dolphin_texCount = 0 ;
+var dolphin_numTextures = dolphin_imgNames.length;
+
+
+var cameraX = 0, cameraY = 0.68, cameraZ = 2.2;
+var lightX = 0, lightY = 5, lightZ = 3;
 var cameraDirX = 0, cameraDirY = 0, cameraDirZ = 1;
 var cube = [];
 var cubeObj = [];
@@ -373,6 +418,8 @@ var newViewDir = new Vector3();
 var staX = 0, staY = 0, staZ = 0;//static camera look at
 
 var moving_camera = 0  ;
+var speed = 1;
+var tempspeed = 0 ;
 var fbo;
 var offScreenWidth = 800, offScreenHeight = 800;
 
@@ -507,6 +554,48 @@ async function main(){
       image.src = fox_imgNames[i];
     }
 
+    response = await fetch('steve/steve.obj');
+    text = await response.text();
+    obj = parseOBJ(text);
+    for( let i=0; i < obj.geometries.length; i ++ ){
+      let o = initVertexBufferForLaterUse(gl, 
+                                          obj.geometries[i].data.position,
+                                          obj.geometries[i].data.normal, 
+                                          obj.geometries[i].data.texcoord);
+      steve_objComponents.push(o);
+    }
+
+    for( let i=0; i < steve_imgNames.length; i ++ ){
+      let image = new Image();
+      image.onload = function(){initTexture(gl, image, steve_imgNames[i], 
+        steve_textures, steve_texCount, steve_numTextures);};
+      image.src = steve_imgNames[i];
+    }
+
+    response = await fetch('dolphin/dolphin.obj');
+    text = await response.text();
+    obj = parseOBJ(text);
+    for( let i=0; i < obj.geometries.length; i ++ ){
+      let o = initVertexBufferForLaterUse(gl, 
+                                          obj.geometries[i].data.position,
+                                          obj.geometries[i].data.normal, 
+                                          obj.geometries[i].data.texcoord);
+      dolphin_objComponents.push(o);
+    }
+
+    for( let i=0; i < dolphin_imgNames.length; i ++ ){
+      let image = new Image();
+      image.onload = function(){initTexture(gl, image, dolphin_imgNames[i], 
+        dolphin_textures, dolphin_texCount, dolphin_numTextures);};
+      image.src = dolphin_imgNames[i];
+    }
+
+
+    foxObj = await loadOBJtoCreateVBO('fox/fox.obj');
+    var normalMapImage = new Image();
+    normalMapImage.onload = function(){initTextureBump(gl, normalMapImage, "normalMapImage");};
+    normalMapImage.src = "normalMap.jpeg";
+
     ////cube
     //TODO-1: create vertices for the cube whose edge length is 2.0 (or 1.0 is also fine)
     //F: Face, T: Triangle
@@ -535,6 +624,14 @@ async function main(){
     fox_modelMatrix = new Matrix4();
     fox_normalMatrix = new Matrix4();
 
+    steve_mvpMatrix = new Matrix4();
+    steve_modelMatrix = new Matrix4();
+    steve_normalMatrix = new Matrix4();
+
+    dolphin_mvpMatrix = new Matrix4();
+    dolphin_modelMatrix = new Matrix4();
+    dolphin_normalMatrix = new Matrix4();
+
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -544,7 +641,7 @@ async function main(){
     // drawAll();
     var tick = function() {
       drawAll();
-      rotating += 1;
+      rotating += 1*speed;
       
       requestAnimationFrame(tick);
     }
@@ -604,9 +701,23 @@ function draw(){
     //Cube (ground)
     //TODO-1: set mdlMatrix for the cube 
     mdlMatrix.setRotate(0, 1, 1, 1);
-    mdlMatrix.scale(3.0, 0.1, 3.0);
-    drawOneObject(cube, mdlMatrix, 0.0, 1.0, 1.0, newViewDir);
+    
+    mdlMatrix.translate(0, -0.1, 0);
+    mdlMatrix.scale(3.0, 0.2, 3.0);
+    drawOneObject(cube, mdlMatrix, 0.0, 0.9, 1.0, newViewDir);
 
+    mdlMatrix.setRotate(0, 1, 1, 1);
+    mdlMatrix.translate(0, -0.02, -0.2);
+    if(rotating%61 <= 30){
+      mdlMatrix.translate(0, (rotating%61-15)/1000, 0)
+    }
+    else{
+      mdlMatrix.translate(0, (60-rotating%61-15)/1000, 0)
+    }
+    mdlMatrix.scale(0.5, 0.2, 0.5);
+    
+    drawOneObject(cube, mdlMatrix, 0.9, 0.73, 0.56, newViewDir);
+    
     // pushMatrix();
 
     // //battleship
@@ -626,27 +737,57 @@ function draw(){
     
     drawOneTextureObject(battleship_modelMatrix,
       battleship_textures, battleship_objComponents, battleship_objComponentIndex, newViewDir);
+    
+    battleship_modelMatrix.translate(0.0, 0.7, 0.0)  
+    battleship_modelMatrix.scale(0.04, 0.04, 0.04)
+    drawOneTextureObject(battleship_modelMatrix,
+      steve_textures, steve_objComponents, steve_objComponentIndex, newViewDir);
+  
+        
+    dolphin_modelMatrix.setRotate(rotating+45, 0, 1, 0);
+    dolphin_modelMatrix.scale(dolphin_objScale*5, dolphin_objScale*5, dolphin_objScale*5);
+    dolphin_modelMatrix.translate(-5.5, -0.0, -0.5);
+    dolphin_modelMatrix.rotate(0, 0, 1, 0);
+    dolphin_modelMatrix.rotate(-15, 1, 0, 0);
+    if(rotating%31 <= 15){
+      dolphin_modelMatrix.rotate((rotating%31-7.5), -1, 0, 0)
+    }
+    else{
+      dolphin_modelMatrix.rotate((30-rotating%31-7.5), -1, 0, 0)
+    }
+    drawOneTextureObject(dolphin_modelMatrix,
+      dolphin_textures, dolphin_objComponents, dolphin_objComponentIndex, newViewDir);
 
     
     //fox
     fox_modelMatrix.setRotate(0, 1, 1, 1);
-    fox_modelMatrix.translate(0, 0.075, 0.0);
+    fox_modelMatrix.rotate(-15, 0, 1, 0)
+    fox_modelMatrix.translate(-0.3, 0.15, 0.0);
+    if(rotating%61 <= 30){
+      fox_modelMatrix.translate(0, (rotating%61-15)/1000, 0)
+    }
+    else{
+      fox_modelMatrix.translate(0, (60-rotating%61-15)/1000, 0)
+    }
     fox_modelMatrix.scale(fox_objScale, fox_objScale, fox_objScale);
     drawOneTextureObject(fox_modelMatrix,
       fox_textures, fox_objComponents, fox_objComponentIndex, newViewDir);
-
-    fox_modelMatrix = new Matrix4();
-    fox_modelMatrix.setRotate(0, 0, 1, 0);
-    fox_modelMatrix.translate(cameraX, cameraY, cameraZ);
-    fox_modelMatrix.rotate(180, 0, 1, 0);
-    fox_modelMatrix.rotate(angleX+180, 0, 1, 0);
-    fox_modelMatrix.rotate(-angleY, 1, 0, 0);
-
-    fox_modelMatrix.scale(fox_objScale*0.5, fox_objScale*0.5, fox_objScale*0.5);
     
-    drawOneTextureObject(fox_modelMatrix,
-      fox_textures, fox_objComponents, fox_objComponentIndex, newViewDir);
-
+    steve_modelMatrix.setRotate(0, 0, 1, 0);
+    steve_modelMatrix.translate(cameraX, cameraY-0.6, cameraZ);
+    steve_modelMatrix.rotate(90, 0, 1, 0);
+    steve_modelMatrix.rotate(angleX+180, 0, 1, 0);
+    steve_modelMatrix.rotate(-angleY, 1, 0, 0);
+    steve_modelMatrix.scale(steve_objScale, steve_objScale, steve_objScale);
+    drawOneTextureObject(steve_modelMatrix,
+      steve_textures, steve_objComponents, steve_objComponentIndex, newViewDir);
+    
+    // steve_modelMatrix.translate(-1, -0.0, 0.6);
+    // steve_modelMatrix.rotate(90, 0, 1, 0);
+    // steve_modelMatrix.scale(steve_objScale*100, steve_objScale*100, steve_objScale*100);
+    // drawOneTextureObject(steve_modelMatrix,
+    //   dolphin_textures, dolphin_objComponents, dolphin_objComponentIndex, newViewDir);
+  
 
     var vpFromCamera = new Matrix4();
     vpFromCamera.setPerspective(60, 1, 1, 15);
@@ -683,6 +824,30 @@ function draw(){
     //normal matrix
     normalMatrix.setInverseOf(modelMatrix);
     normalMatrix.transpose();
+    //stone fox
+    let vpMatrix = new Matrix4();
+    vpMatrix.setPerspective(60, 1, 1, 15);
+    if(moving_camera == 0){
+      vpMatrix.lookAt(3, 2, 4, staX, staY, staZ, 0, 1, 0)
+    }
+    else{
+      vpMatrix.lookAt(cameraX, cameraY, cameraZ, 
+        cameraX + newViewDir.elements[0], 
+        cameraY + newViewDir.elements[1], 
+        cameraZ + newViewDir.elements[2], 
+        0, 1, 0);
+    }
+    mdlMatrix.setRotate(0, 1, 0, 0);//for mouse rotation
+    mdlMatrix.rotate(15, 0, 1, 0);//for mouse rotation
+    mdlMatrix.translate(0.3, 0.15, 0.0)
+    if(rotating%61 <= 30){
+      mdlMatrix.translate(0, (rotating%61-15)/1000, 0)
+    }
+    else{
+      mdlMatrix.translate(0, (60-rotating%61-15)/1000, 0)
+    }
+    mdlMatrix.scale(fox_objScale, fox_objScale, fox_objScale);
+    drawOneRegularObject(foxObj, mdlMatrix, vpMatrix, 0.5, 0.5, 0.5);
 
     //quad
     gl.useProgram(programEnvCube);
@@ -702,6 +867,12 @@ function draw(){
 function drawOneObject(obj, mdlMatrix, colorR, colorG, colorB, newViewDir){
     //model Matrix (part of the mvp matrix)
     gl.useProgram(program);
+
+    let modelMatrix = new Matrix4();
+    let mvpMatrix = new Matrix4();
+    let normalMatrix = new Matrix4();
+    let lightMatrix = new Matrix4();
+    
     modelMatrix.setRotate(0, 1, 0, 0);//for mouse rotation
     modelMatrix.multiply(mdlMatrix);
     //mvp: projection * view * model matrix  
@@ -730,7 +901,7 @@ function drawOneObject(obj, mdlMatrix, colorR, colorG, colorB, newViewDir){
     gl.uniform3f(program.u_ViewPosition, cameraX, cameraY, cameraZ);
     gl.uniform1f(program.u_Ka, 0.2);
     gl.uniform1f(program.u_Kd, 0.7);
-    gl.uniform1f(program.u_Ks, 1.0);
+    gl.uniform1f(program.u_Ks, 0.0);
     gl.uniform1f(program.u_shininess, 10.0);
     gl.uniform3f(program.u_Color, colorR, colorG, colorB);
 
@@ -1004,6 +1175,22 @@ function keydown(ev){
   else if(ev.key == 'd'){
     cameraY -= 0.01;
   }
+  else if(ev.key == 'r'){
+    cameraX = 0;
+    cameraY = 0.68;
+    cameraZ = 2.2;
+  }
+  else if(ev.key == '+'){
+    speed += 0.1;
+  }
+  else if(ev.key == '-'){
+    speed -= 0.1;
+  }
+  else if(ev.key == 'p'){
+    var temp = tempspeed;
+    tempspeed = speed;
+    speed = temp;
+  }
 
   console.log(cameraX, cameraY, cameraZ)
   drawAll();
@@ -1130,6 +1317,9 @@ function drawOneTextureObject(_modelMatrix, textures, objComponents, objComponen
   gl.uniform1f(programTexture.u_Ka, 0.2);
   gl.uniform1f(programTexture.u_Kd, 0.7);
   gl.uniform1f(programTexture.u_Ks, 1.0);
+  if(textures == steve_textures){
+    gl.uniform1f(programTexture.u_Ks, 0.0)
+  }
   gl.uniform1f(programTexture.u_shininess, 10.0);
   gl.uniformMatrix4fv(programTexture.u_MvpMatrix, false, _mvpMatrix.elements);
   gl.uniformMatrix4fv(programTexture.u_modelMatrix, false, _modelMatrix.elements);
@@ -1143,4 +1333,141 @@ function drawOneTextureObject(_modelMatrix, textures, objComponents, objComponen
     initAttributeVariable(gl, programTexture.a_Normal, objComponents[i].normalBuffer);
     gl.drawArrays(gl.TRIANGLES, 0, objComponents[i].numVertices);
   }
+}
+
+function drawOneRegularObject(obj, modelMatrix, vpMatrix, colorR, colorG, colorB){
+  gl.useProgram(programBump);
+  let mvpMatrix = new Matrix4();
+  let normalMatrix = new Matrix4();
+  mvpMatrix.set(vpMatrix);
+  mvpMatrix.multiply(modelMatrix);
+
+  //normal matrix
+  normalMatrix.setInverseOf(modelMatrix);
+  normalMatrix.transpose();
+
+  gl.uniform3f(programBump.u_LightPosition, lightX, lightY, lightZ);
+  gl.uniform3f(programBump.u_ViewPosition, cameraX, cameraY, cameraZ);
+  gl.uniform1f(programBump.u_Ka, 0.2);
+  gl.uniform1f(programBump.u_Kd, 0.7);
+  gl.uniform1f(programBump.u_Ks, 1.0);
+  gl.uniform1f(programBump.u_shininess, 40.0);
+  gl.uniform3f(programBump.u_Color, colorR, colorG, colorB);
+  gl.uniform1i(programBump.u_Sampler0, 0);
+  gl.uniform1i(programBump.u_Sampler1, 1);
+
+  gl.uniformMatrix4fv(programBump.u_MvpMatrix, false, mvpMatrix.elements);
+  gl.uniformMatrix4fv(programBump.u_modelMatrix, false, modelMatrix.elements);
+  gl.uniformMatrix4fv(programBump.u_normalMatrix, false, normalMatrix.elements);
+
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, textures["normalMapImage"]);
+
+  for( let i=0; i < obj.length; i ++ ){
+    initAttributeVariable(gl, programBump.a_Position, obj[i].vertexBuffer);
+    initAttributeVariable(gl, programBump.a_Normal, obj[i].normalBuffer);
+    initAttributeVariable(gl, programBump.a_TexCoord, obj[i].texCoordBuffer);
+    initAttributeVariable(gl, programBump.a_Tagent, obj[i].tagentsBuffer);
+    initAttributeVariable(gl, programBump.a_Bitagent, obj[i].bitagentsBuffer);
+    initAttributeVariable(gl, programBump.a_crossTexCoord, obj[i].crossTexCoordsBuffer);
+    gl.drawArrays(gl.TRIANGLES, 0, obj[i].numVertices);
+  }
+}
+
+function calculateTangentSpace(position, texcoord){
+  //iterate through all triangles
+  let tagents = [];
+  let bitagents = [];
+  let crossTexCoords = [];
+  for( let i = 0; i < position.length/9; i++ ){
+    let v00 = position[i*9 + 0];
+    let v01 = position[i*9 + 1];
+    let v02 = position[i*9 + 2];
+    let v10 = position[i*9 + 3];
+    let v11 = position[i*9 + 4];
+    let v12 = position[i*9 + 5];
+    let v20 = position[i*9 + 6];
+    let v21 = position[i*9 + 7];
+    let v22 = position[i*9 + 8];
+    let uv00 = texcoord[i*6 + 0];
+    let uv01 = texcoord[i*6 + 1];
+    let uv10 = texcoord[i*6 + 2];
+    let uv11 = texcoord[i*6 + 3];
+    let uv20 = texcoord[i*6 + 4];
+    let uv21 = texcoord[i*6 + 5];
+
+    let deltaPos10 = v10 - v00;
+    let deltaPos11 = v11 - v01;
+    let deltaPos12 = v12 - v02;
+    let deltaPos20 = v20 - v00;
+    let deltaPos21 = v21 - v01;
+    let deltaPos22 = v22 - v02;
+
+    let deltaUV10 = uv10 - uv00;
+    let deltaUV11 = uv11 - uv01;
+    let deltaUV20 = uv20 - uv00;
+    let deltaUV21 = uv21 - uv01;
+
+    let r = 1.0 / (deltaUV10 * deltaUV21 - deltaUV11 * deltaUV20);
+    for( let j=0; j< 3; j++ ){
+      crossTexCoords.push( (deltaUV10 * deltaUV21 - deltaUV11 * deltaUV20) );
+    }
+    let tangentX = (deltaPos10 * deltaUV21 - deltaPos20 * deltaUV11)*r;
+    let tangentY = (deltaPos11 * deltaUV21 - deltaPos21 * deltaUV11)*r;
+    let tangentZ = (deltaPos12 * deltaUV21 - deltaPos22 * deltaUV11)*r;
+    for( let j = 0; j < 3; j++ ){
+      tagents.push(tangentX);
+      tagents.push(tangentY);
+      tagents.push(tangentZ);
+    }
+    let bitangentX = (deltaPos20 * deltaUV10 - deltaPos10 * deltaUV20)*r;
+    let bitangentY = (deltaPos21 * deltaUV10 - deltaPos11 * deltaUV20)*r;
+    let bitangentZ = (deltaPos22 * deltaUV10 - deltaPos12 * deltaUV20)*r;
+    for( let j = 0; j < 3; j++ ){
+      bitagents.push(bitangentX);
+      bitagents.push(bitangentY);
+      bitagents.push(bitangentZ);
+    }
+  }
+  let obj = {};
+  obj['tagents'] = tagents;
+  obj['bitagents'] = bitagents;
+  obj['crossTexCoords'] = crossTexCoords;
+  return obj;
+}
+
+async function loadOBJtoCreateVBO( objFile ){
+  let objComponents = [];
+  response = await fetch(objFile);
+  text = await response.text();
+  obj = parseOBJ(text);
+  for( let i=0; i < obj.geometries.length; i ++ ){
+    let tagentSpace = calculateTangentSpace(obj.geometries[i].data.position, 
+                                            obj.geometries[i].data.texcoord);
+    let o = initVertexBufferForLaterUseBump(gl, 
+                                        obj.geometries[i].data.position,
+                                        obj.geometries[i].data.normal, 
+                                        obj.geometries[i].data.texcoord,
+                                        tagentSpace.tagents,
+                                        tagentSpace.bitagents,
+                                        tagentSpace.crossTexCoords);
+    objComponents.push(o);
+  }
+  return objComponents;
+}
+
+function initTextureBump(gl, img, texKey){
+  var tex = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, tex);
+
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+
+  textures[texKey] = tex;
+
+  texCount++;
+  if( texCount == numTextures)drawAll();
 }
